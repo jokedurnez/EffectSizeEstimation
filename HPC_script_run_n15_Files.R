699145d664ab55482136e79caac1ca35a2c9a776
Bieke Moerkerke <bmoerker@vpna244.ugent.be>
2016-04-13 00:19:23 -0700

    Vary sample size 2

---
+++ HPC_script_run_n15_Files.R
@@ -0,0 +1,9 @@
+HPCfolder <- "/user/data/gent/gvo000/gvo00022/00_data/05_motor/"
+scratchdir <- "/user/scratch/gent/vsc418/vsc41855/Tempresultsn15/"
+resultsdir<-"/user/scratch/gent/vsc418/vsc41855/resultsn15/"
+
+
+setwd(scratchdir)
+
+write(c("Iteration","Study","Peakt","Peak","WPeak","Cluster","Significant","ESC","WESC","PES","xcord","ycord","zcord"),paste(resultsdir,"results.txt",sep=""),ncolumns=12)
+write(c("Iteration","Cluster","Peakz","xcord","ycord","zcord","Peakt"),paste(resultsdir,"peakGT.txt",sep=""),ncolumns=6)
